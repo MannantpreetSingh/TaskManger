7847f330a6cfdf4f1712638e8e8898e442dacfeb
Mannatpreet Singh <mannatpreetsingh14@outlook.com>
2026-08-07 20:11:06 +0530

    fixed bugs in index.php

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,20 +1,21 @@
 <?php
 session_start();
 include("../config/db.php");
-if(!isset($_SESSION["user_id"])){
-    header("location: ../auth/login.php" );
+if (!isset($_SESSION["user_id"])) {
+    header("location: ../auth/login.php");
     exit();
 }
 $user_id = $_SESSION["user_id"];
-$sql = " SELECT * FROM tasks WHERE user_id ='$user_id'";
+echo "Logged in user_id: " . $user_id . "<br>";
+$sql = " SELECT* FROM tasks WHERE user_id ='$user_id'";
 $result = $conn->query($sql);
 ?>
 <h2> Your Taks </h2>
-<a href = "../tasks/create_task.php ">
-    Add new tasks 
+<a href="../tasks/create_task.php ">
+    Add new tasks
 </a>
 <?php
-while($row = $result->fetch_assoc()){
-    echo $row["title"]. "<br>";
+while ($row = $result->fetch_assoc()) {
+    echo $row['task'] . "<br>";
 }
 ?>
