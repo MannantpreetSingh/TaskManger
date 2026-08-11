38bb701fe593bc232febbbd81132ca60a65c9f58
Mannatpreet Singh <mannatpreetsingh14@outlook.com>
2026-08-11 20:04:20 +0530

    connect website to Aiven MySQL

--- config/db.php
+++ config/db.php
@@ -1,7 +1,31 @@
 <?php
 
-$conn = new mysqli("127.0.0.1", "root", "", "taskmanger", 3307);
-if ($conn->connect_error) {
-    die("connection failed " . $conn->connect_error);
+$host = getenv("DB_HOST");
+$port = getenv("DB_PORT");
+$user = getenv("DB_USER");
+$password = getenv("DB_PASSWORD");
+$database = getenv("DB_NAME");
+$conn = mysqli_init();
+mysqli_ssl_set(
+    $conn,
+    null,
+    null,
+    "/etc/secrets/ca.pem",
+    null,
+    null
+);
+mysqli_real_connect(
+    $conn,
+    $host,
+    $user,
+    $password,
+    $database,
+    $port,
+    null,
+    MYSQLI_CLIENT_SSL
+);
+if (!$conn) {
+    die("connection failed " . mysqli_connect_error());
+    
 }
 ?>
